68ba937f02bf366ff62f219cc6bb737f092907ae
Panos Daskalopoulos <115027160+panos1b@users.noreply.github.com>
2024-07-12 17:10:57 +0300

    Update HomeController.cs
    Fixed Formating

--- fourth-exersize/Controllers/HomeController.cs
+++ fourth-exersize/Controllers/HomeController.cs
@@ -17,10 +17,10 @@ namespace fourth_exersize.Controllers
 			return View();
 		}
 
-        public IActionResult NotFound()
-        {
-            return View();
-        }
+        	public IActionResult NotFound()
+        	{
+           	 	return View();
+        	}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
